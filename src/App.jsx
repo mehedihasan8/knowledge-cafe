d33631bd876f0f333ff6bb5c82f63b29fb09cc87
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2023-03-30 23:25:48 +0600

    fake data create done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import "./App.css";
 import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <div className="App">
       <Header></Header>
+      <hr />
     </div>
   );
 };
