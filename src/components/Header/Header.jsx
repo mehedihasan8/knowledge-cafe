b88104a01c01a2805ae54fd7924ae9a2b88bd569
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2023-04-01 20:35:20 +0600

    over all project done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,11 +4,11 @@ import "./Header.css";
 const Header = () => {
   return (
     <nav className="header-container">
-      <h1 className="nav-title">Knowledge Cafe</h1>
+      <h1 className="nav-title">Programmign Cafe</h1>
       <div className="header-items">
         <a href="/home">Home</a>
         <a href="/blog">Blog</a>
-        <a href="/joint">Joint</a>
+        <a href="/details">Details</a>
         <a href="/about">About</a>
       </div>
     </nav>
